e115fef7a6045d435ac08da6dc909e1f1e034aba
smleandro <leandrosolismora@estudiantec.cr>
2022-01-25 00:43:14 -0600

    login implementation

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
 import "./login.css"
+import loginImg from '../../assets/imgs/loginImg.svg'
+
+
 
 const Login = () => {
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState("")
+
     return (
         <div className="login-general-div">
-            <h1 className="login-form-title">Soda Gallitos</h1>
-            <h1 className="login-form-title">Login</h1>
+            <div className="login-column" style={{ backgroundImage: `url(${loginImg})` }}>
+                <h1 className="login-banner-title">Soda Gallitos</h1>
+                <h2 className="login-banner-subtitle">Administrativo</h2>
+            </div>
+
+            <div className="login-column">
+                <div className="login-form-container">
+                    <div className="login-form-container-inputs">
+                        <div className="login-form-column">
+                            <h2 className="login-form-text">Nombre de usuario:</h2>
+                            <h2 className="login-form-text">Contraseña:</h2>
+                        </div>
+                        <div className="login-form-column">
+                            <input type="text"
+                                className="login-input"
+                                value={password}
+                                onChange={(evt) => { setPassword(evt.target.value) }}
+                            />
+                            <input type="password"
+                                className="login-input"
+                                value={user}
+                                onChange={(evt) => { setUser(evt.target.value) }}
+                            />
+                        </div>
+                    </div>
+                    
+                    <a href="/home">
+                        <button className="login-button">
+                            Iniciar sesión
+                        </button>
+                    </a>
+
+                </div>
+            </div>
+
         </div>
     )
 }
